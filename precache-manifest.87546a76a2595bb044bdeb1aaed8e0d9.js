df5b7768e3f6dce0473156206945be290fc49067
ArturVargas <arturvargas9226@gmail.com>
2019-09-27 19:16:44 -0500

    Updates

---
+++ precache-manifest.87546a76a2595bb044bdeb1aaed8e0d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3904d9061be1221c203c9bc9a8136f79",
+    "url": "/ArturVargas/React-Monsters/index.html"
+  },
+  {
+    "revision": "a36a491c333efe480f25",
+    "url": "/ArturVargas/React-Monsters/static/css/main.245f252a.chunk.css"
+  },
+  {
+    "revision": "783bb2d55f15e42d1998",
+    "url": "/ArturVargas/React-Monsters/static/js/2.f3e65479.chunk.js"
+  },
+  {
+    "revision": "a36a491c333efe480f25",
+    "url": "/ArturVargas/React-Monsters/static/js/main.3aee561a.chunk.js"
+  },
+  {
+    "revision": "02a65fe19717f41765e3",
+    "url": "/ArturVargas/React-Monsters/static/js/runtime-main.162cfb2c.js"
+  }
+]);
